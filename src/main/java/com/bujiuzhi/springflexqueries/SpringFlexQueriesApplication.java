6050fbe4db5ed52cd30d70b9d456e1950d21963f
bujiuzhi <bujiuzhi@gmail.com>
2023-11-17 19:23:17 +0800

    first commit

---
+++ src/main/java/com/bujiuzhi/springflexqueries/SpringFlexQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.bujiuzhi.springflexqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlexQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlexQueriesApplication.class, args);
+    }
+
+}
